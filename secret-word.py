377e8f6dbe01f539d916d22a1e122786f1a89109
Danielle <159485441+dmorrison15@users.noreply.github.com>
2025-03-05 20:23:41 -0500

    Modularize functions

--- secret-word.py
+++ secret-word.py
@@ -3,17 +3,55 @@ import random
 random.seed(1)
 
 guessed_letters = []
-
+def go():
+    greet_player()
+    start_game()
+    thank_player()
+def start_game():
+    while True:
+        answer = input("Would you like to start a new round? (Please enter yes or no)\n").strip()
+        if answer.lower() == "yes":
+            play_round()
+        elif answer.lower() == "no":
+            break
+        else:
+            print("Invalid input. Please try again\n")
 def greet_player():
     print("\n\nWelcome to secret word! In this game, you will be guessing a random word with 5 lives to guess a letter in the word. For each incorrect guess, you will lose a life. If you guess the word correctly, you win! If you run out of lives, you lose.\n")
-
-# Returns true if the guess is correct, and false if the guess is incorrect
-def guess_letter(secret_word, displayed_word, numlives):
-    print("")
-    # Get player's guess
-    guess = input("Guess a letter: ").strip()
-    
-    # Make sure guess is valid and hasn't been guessed already
+def guess_letter(secret_word, displayed_word):
+    guess = verify_guess(get_player_guess(), guessed_letters)
+    guessed_letters.append(guess)
+    return check_guess(guess, secret_word, displayed_word)          
+def check_guess(guess, secret_word, displayed_word):
+    if guess in secret_word:
+        reveal_letters(guess, secret_word, displayed_word)
+        print("Good guess!")
+        return True
+    else:
+        print(guess, "is not in the secret word.")
+        return False 
+def get_player_guess():
+    guess = input("\nGuess a letter: ")
+    guess = guess.strip()
+    return guess.lower()
+def play_round():
+    secret_word = get_secret_word()
+    numlives = 5
+    displayed_word = []
+    display_blank_word(secret_word, displayed_word)
+    while numlives > 0:
+        numlives = update_lives(secret_word, displayed_word, numlives)
+        if player_wins(displayed_word, secret_word):    
+            break
+        display_word(displayed_word)
+        display_lives(numlives)
+    else:    
+        game_over(numlives, secret_word)
+def reveal_letters(guess, secret_word, displayed_word):
+    for i in range(len(secret_word)):
+            if secret_word[i] == guess:
+                displayed_word[i] = secret_word[i]
+def verify_guess(guess, guessed_letters):
     while True:
         if len(guess) != 1 or not guess.isalpha():
             guess = input("Guess is invalid - please try again: ")
@@ -22,63 +60,38 @@ def guess_letter(secret_word, displayed_word, numlives):
             guess = input("You already guessed this letter - please try again: ")
             continue
         break
-    guessed_letters.append(guess)
-    guess = guess.lower()
-    # If guess is correct, reveal where the letter is in the word
-    if guess in secret_word:
-        for i in range(len(secret_word)):
-            if secret_word[i] == guess:
-                displayed_word[i] = secret_word[i]
-        print("Good guess!")
-        # print(" ".join(displayed_word))
-        return True
-    # If the guess is wrong, player loses a life
+    return guess  
+def update_lives(secret_word, displayed_word, numlives):
+    if guess_letter(secret_word, displayed_word) == False:
+        numlives-=1
+    return numlives
+def display_lives(numlives):
+    if numlives == 1:
+        print("\nYou have 1 life left")
     else:
-        print(guess, "is not in the secret word.")
-        # print(" ".join(displayed_word)) 
-        return False          
-
-def play_round():
-# Import list of words from text file and select random word
+        print("\nYou have", numlives, "lives left")
+def player_wins(displayed_word, secret_word):
+    if "_" not in displayed_word:
+            print("\nYou win! The word is", secret_word)
+            return True
+    return False
+def get_secret_word():
     with open("/home/danimorrison15/secret-word/linuxwords.txt") as f:
         words = f.read().splitlines()
-    secret_word = random.choice(words) #cohered
-    
-    numlives = 5
-    
-    displayed_word = []
+    return random.choice(words)
+def game_over(numlives, secret_word):
+    if numlives == 0:
+        print("\nYou lose, the word was", secret_word, "- Better luck next time")
+def thank_player():
+    print("Thanks for playing!")
+def display_blank_word(secret_word, displayed_word):
     for letter in secret_word:
         displayed_word.append("_")
+    display_word(displayed_word)    
+def test():
+    pass
+def display_word(displayed_word):
     print(" ".join(displayed_word))
-    print("")
-    
-    while numlives > 0:
-        if guess_letter(secret_word, displayed_word, numlives) == False:
-            numlives-=1
-        if "_" not in displayed_word:
-            print("You win! The word is", secret_word)
-            break
-        print(" ".join(displayed_word))
-        if numlives == 1:
-            print("You have 1 life left")
-        else:
-            print("You have", numlives, "lives left")
-    if numlives == 0:
-        print("You lose, the word was", secret_word, "- Better luck next time")
-
-
-greet_player()
-
-while True:
-    answer = input("Would you like to start a new round? (Please enter yes or no)\n")
-    if answer.lower() == "yes":
-        play_round()
-    elif answer.lower() == "no":
-        break
-    else:
-        print("Invalid input. Please try again\n")
-
-print("Thanks for playing!")
-    
-
+test()
+go()
 
